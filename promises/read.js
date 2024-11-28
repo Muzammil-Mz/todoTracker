05b5d78bc13efde025637afa263dcfeed5567c2c
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-28 06:08:26 +0000

    28 nov

---
+++ promises/read.js
@@ -0,0 +1,13 @@
+// import fs from "fs/promises"
+
+// async function readingFile() {
+//     try{
+//         let readData=await fs.readFile("./suhail.txt","utf-8")
+    
+//     console.log(readData);
+// } catch (error){
+//     console.log(error);
+    
+// }
+// }
+// readingFile()
